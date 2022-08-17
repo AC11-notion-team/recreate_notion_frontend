71cdaef0321c01d6cd3c1457df95f75da3904f20
luna <qqa0705@gmail.com>
2022-08-17 10:13:30 +0800

    add Navbar

---
+++ src/Components/Navbar/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Share from "./Share"
+import Updata from "./Updata"
+import Star from "./Star"
+import More from "./More"
+import clock from "../image/clock.png"
+import emptyStar from "../image/empty-star.jpeg"
+import fullStar from "../image/full-star.png"
+import more from "../image/more.png"
+
+
+export default function Header(){
+    return(
+       <div className="flex  px-4 leading-10 ">
+           <div className="flex ">
+                <div className="mr-2">
+                    側邊欄選單
+                </div>
+                <div>
+                    <img src="" />
+                    <p>標題</p>
+                </div>
+           </div>
+            <div className="flex ">
+                <div className="mx-2">
+                    share
+                    <Share />
+                </div>
+                <div className="mx-2 flex items-center">
+                    <img className="w-5 "  src={clock} alt="updata" />
+                    <Updata />
+                </div>
+                <div className="mx-2 flex items-center">
+                    <img className="w-5" src={emptyStar}  />
+                    <Star />
+                </div>
+                <div className="mx-2 flex items-center"> 
+                    <img className="w-5 " src={more}  />
+                    <More />
+                </div>  
+            </div>
+       </div> 
+    )
+}
---
+++ src/Components/Navbar/Updata.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Updata(){
+    return(
+        <div></div>
+    )
+}
