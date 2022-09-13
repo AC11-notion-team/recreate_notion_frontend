32f74d8a0b1dcc93fb28624979189986f36b9145
luna <qqa0705@gmail.com>
2022-09-13 12:45:31 +0800

    新增PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    plugins: [
+      purgecss({
+        content: ['./**/*.html']
+      }),
+    ]
   },
 }
