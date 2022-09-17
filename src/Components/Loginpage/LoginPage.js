6b1a534984fe6377bcf3f4fdb173af601a0c6e80
Yin <Gingkochang@gmail.com>
2022-09-18 00:11:15 +0800

    新增logo href,修改contactSales href,新增gitatrributes檔案

--- src/Components/Loginpage/LoginPage.js
+++ src/Components/Loginpage/LoginPage.js
@@ -145,9 +145,12 @@ export default function LoginPage() {
   
   return (
     <>
-      <div className="flex items-center justify-items-start mt-6 ml-6 h-12 w-10 "><img src="/zettel.png" alt="" />
-        <span className="text-xl font-medium ml-2">Zettel</span>
-      </div>
+      <a href='homepage'>
+        <div className="flex items-center justify-items-start mt-6 ml-6 h-12 w-10 ">
+          <img src="/zettel.png" alt="" />
+          <span className="text-xl font-medium ml-2">Zettel</span>
+        </div>
+      </a>
       <div className="w-full mt-4 border-b-2  border-grey-100 "/>
        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
