e3a01ffe4a67b0025b31e475fce27634e9ae24a1
Yin <Gingkochang@gmail.com>
2022-09-22 11:08:23 +0800

    修正帶入值

--- src/Components/Sidebar/QuickFind.js
+++ src/Components/Sidebar/QuickFind.js
@@ -14,12 +14,11 @@ export default function QuickFind(){
             setIsQuickFind(prevQuickFind => !prevQuickFind)
         }
     };
-    const [isSearchText,setIsSearchText] = useState("")
+    const [searchText,setSearchText] = useState("")
     const baseUrl = process.env.REACT_APP_BASEURL
-    const [isKeyword,setIskeyword] = useState([])
+    const [Keyword,setkeyword] = useState([])
     const handleChange = (event) => {
-        setIsSearchText(event.target.value)
-        console.log(event.target.value);
+        setSearchText(event.target.value)
         axios({
             method:"get",
             url:`${baseUrl}/users/search_page.json`,
@@ -31,7 +30,7 @@ export default function QuickFind(){
                 search: event.target.value
             }
             }).then((res)=>{
-            setIskeyword(res.data.pages)
+            setkeyword(res.data.pages)
             console.log(res.data.pages)
             }).catch((err) => {
                 console.log(err);
@@ -39,10 +38,9 @@ export default function QuickFind(){
     };
 
     function page (pageid){
-        console.log(pageid);
         changeCurrentPageId(pageid)
         setIsQuickFind(false)
-        setIskeyword([])
+        setkeyword([])
         
     }
             
@@ -69,14 +67,14 @@ export default function QuickFind(){
             {isQuickFind && <div onClick={handleToggle}  className="QuickFind absolute bg-gray-500 bg-opacity-30 w-screen top-0 bottom-0 left-0 flex-grow">
                 <div className="relative  w-6/12 h-3/5 m-auto top-40 left-10 right-10 bg-white opacity-100 rounded-lg">
                     <div className="px-5 py-3 flex items-center">
-                        <img className="w-5 h-5 mr-3" src={search} alt="seach" value={isSearchText} />
-                        <input type="text" placeholder="Search username's Notion..." onChange={handleChange} className="focus:outline-none w-full text-xl"/>
+                        <img className="w-5 h-5 mr-3" src={search} alt="seach" value={searchText} />
+                        <input type="text" placeholder="Search username's Notion..." onChange={handleChange} className="focus:outline-none w-full text-xl" value={searchText}/>
                     </div>
                     <hr />
                     <div className="h-4/6 overflow-x-hidden overflow-y-auto px-1 py-1">
                         <div className="mb-3">
                             {
-                                isKeyword?.map((page)=>{
+                                Keyword?.map((page)=>{
                                     return <SearchPageList pageTitle={page.title} pageIcon={page.icon} key={page.id} pageId={page.id} />
                                 })
                             }
