316dab843703b2dd6dc9c7c5e7157d2bdb2df05e
luna <qqa0705@gmail.com>
2022-09-22 20:14:54 +0800

    首頁修正

--- src/Components/PageHeader/PageHeader.js
+++ src/Components/PageHeader/PageHeader.js
@@ -88,7 +88,7 @@ function PageHeader({ onEmojiClick }) {
 					<input
 						id="coverImg"
 						type="file"
-						className="hidden block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+						className="hidden w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
 						onChange={upload}
 					/>
 				</label>
