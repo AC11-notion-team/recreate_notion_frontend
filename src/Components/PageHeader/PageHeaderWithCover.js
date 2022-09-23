812c8e55bde0aea4208b5750e8f4bf22735f8120
Juttiz <juttiz1992@gmail.com>
2022-09-23 17:18:29 +0800

    hover changecover button

--- src/Components/PageHeader/PageHeaderWithCover.js
+++ src/Components/PageHeader/PageHeaderWithCover.js
@@ -1,69 +1,39 @@
 import React from 'react'
+import CircleLoader from "react-spinners/CircleLoader";
 
-function PageHeaderWithCover(){
+function PageHeaderWithCover({isChangeCover, cover, loading}){
+    const override = {
+		display: "block",
+		margin: "auto",
+		borderColor: "red",
+        position: "absolute",
+        bottom: "6rem",
+        top: "6rem",
+        left: "6rem",
+        right: "6rem",
+
+	};
     return (
-        // <div className="group">
-        //     <div className="relative ">
-        //         <label
-        //             htmlFor="coverImg"
-        //             className="absolute px-1 py-0.5 text-sm bg-white border rounded right-5 bottom-2 z-10"
-        //         >
-        //             change cover
-        //             <input
-        //                 id="coverImg"
-        //                 type="file"
-        //                 className="hidden w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-        //                 onChange={upload}
-        //             />
-        //         </label>
+        <div className="relative w-full h-48" >
+            {isChangeCover && (
+                <label
+                    htmlFor="coverImg"
+                    className="absolute px-1 py-0.5 text-sm bg-white text-black border rounded right-5 bottom-2 z-10"
+                >change cover</label>
+            )}
+            <img
+                src={cover}
+                className="object-cover w-full h-full"
+                alt="cover"
+            />
+            <CircleLoader
+                color="#d69636"
+                loading={loading}
+                cssOverride={override}
+                size={50}
+            />
+        </div>
+    )
+}
 
-        //         <section class="w-full relative ">
-        //             {cover && (
-        //                 <img
-        //                     src={cover}
-        //                     className="object-cover w-full h-48 overflow-hidden"
-        //                     alt="cover"
-        //                 />
-        //             )}
-
-        //             <CircleLoader
-        //                 color="#d69636"
-        //                 loading={loading}
-        //                 cssOverride={override}
-        //                 size={50}
-        //             />
-
-        //             {cover && (
-        //                 <div className="absolute mb-2 ml-3 text-6xl cursor-pointer left-44 -bottom-7">
-        //                     <Emoji
-        //                         currentPageID={currentPageId}
-        //                         pageIcon={pageIcon}
-        //                         onEmojiClick={onEmojiClick}
-        //                     />
-        //                 </div>
-        //             )}
-        //         </section>
-        //     </div>
-        //     <div className="w-auto mx-20 mt-8 mb-16">
-        //         <div className="relative flex items-center w-4/6 max-w-screen-sm m-auto">
-        //             {!cover && (
-        //                 <div className="mb-2 mr-3 text-4xl cursor-pointer left-44 -bottom-7">
-        //                     <Emoji
-        //                         currentPageID={currentPageId}
-        //                         pageIcon={pageIcon}
-        //                         onEmojiClick={onEmojiClick}
-        //                     />
-        //                 </div>
-        //             )}
-
-        //             <input
-        //                 className="w-3/4 text-4xl font-bold outline-none"
-        //                 placeholder="Untitled"
-        //                 onChange={(event) => onEmojiClick(event, currentPageId)}
-        //                 id={currentPageId}
-        //                 value={pageTitle || ""}
-        //             />
-        //         </div>
-        //     </div>
-        // </div>
-)}
+export default PageHeaderWithCover
